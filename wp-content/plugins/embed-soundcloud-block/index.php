9333c3102d84be5eeab3a6e09ca7648eea69bfc6
Totopo27 <134097316+ByronLatouche@users.noreply.github.com>
2023-05-21 15:38:57 -0600

    agregado soundcloud

---
+++ wp-content/plugins/embed-soundcloud-block/index.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Plugin Name: Embed SoundCloud
+ * Description: Embed your soundCloud tracks with a beautiful Gutenberg block . 
+ * Version: 1.0.1
+ * Author: bPlugins LLC
+ * Author URI: http://bplugins.com
+ * License: GPLv3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain: sound-cloud
+ */
+
+// ABS PATH
+if ( !defined( 'ABSPATH' ) ) { exit; }
+
+// Constant
+define( 'SCB_PLUGIN_VERSION', 'localhost' === $_SERVER['HTTP_HOST'] ? time() : '1.0.1' );
+define( 'SCB_ASSETS_DIR', plugin_dir_url( __FILE__ ) . 'assets/' );
+ 
+// SoundCloud Directory
+class SCBSoundCloud{
+	function __construct(){
+		add_action( 'enqueue_block_assets', [$this, 'enqueueBlockAssets'] );
+		add_action( 'init', [$this, 'onInit'] );
+	}
+
+	function has_reusable_block( $block_name ){
+		if( get_the_ID() ){
+			if ( has_block( 'block', get_the_ID() ) ){
+				// Check reusable blocks
+				$content = get_post_field( 'post_content', get_the_ID() );
+				$blocks = parse_blocks( $content );
+	
+				if ( !is_array( $blocks ) || empty( $blocks ) ) {
+					return false;
+				}
+	
+				foreach ( $blocks as $block ) {
+					if ( $block['blockName'] === 'core/block' && ! empty( $block['attrs']['ref'] ) ) {
+						if( has_block( $block_name, $block['attrs']['ref'] ) ){
+						 	return true;
+						}
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	function enqueueBlockAssets(){
+		wp_register_script( 'soundCloud', SCB_ASSETS_DIR . 'js/soundCloud.api.js', [], SCB_PLUGIN_VERSION );
+
+		wp_register_script( 'scb-sound-cloud-script', plugins_url( 'dist/script.js', __FILE__ ), ['react', 'react-dom', 'soundCloud'], SCB_PLUGIN_VERSION );
+
+		wp_register_style( 'scb-sound-cloud-style', plugins_url( 'dist/style.css', __FILE__ ), [], SCB_PLUGIN_VERSION ); // Frontend Style
+
+		if( $this->has_reusable_block( 'scb/sound-cloud' ) || has_block( 'scb/sound-cloud', get_the_ID() ) ){
+			wp_enqueue_style( 'scb-sound-cloud-style' );
+			wp_enqueue_script( 'scb-sound-cloud-script' );
+		}
+	}
+
+	function onInit() {
+		wp_register_style( 'scb-sound-cloud-editor-style', plugins_url( 'dist/editor.css', __FILE__ ), [ 'wp-edit-blocks','scb-sound-cloud-style' ], SCB_PLUGIN_VERSION ); // Backend Style
+
+		register_block_type( __DIR__, [
+			'editor_style'		=> 'scb-sound-cloud-editor-style',
+			'render_callback'	=> [$this, 'render']
+		] ); // Register Block
+
+		wp_set_script_translations( 'scb-sound-cloud-editor-script', 'sound-cloud', plugin_dir_path( __FILE__ ) . 'languages' ); // Translate
+	}
+
+	function render( $attributes ){
+		extract( $attributes );
+
+		$className = $className ?? '';
+		$scbBlockClassName = 'wp-block-scb-sound-cloud ' . $className . ' align' . $align;
+
+		ob_start(); ?>
+		<div class='<?php echo esc_attr( $scbBlockClassName ); ?>' id='scbSoundCloud-<?php echo esc_attr( $cId ) ?>' data-attributes='<?php echo esc_attr( wp_json_encode( $attributes ) ); ?>'></div>
+
+		<?php return ob_get_clean();
+	} // Render
+}
+new SCBSoundCloud();
